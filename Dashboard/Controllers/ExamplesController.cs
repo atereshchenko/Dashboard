1209b98c5cd0a99392c074db36d96535942f9ee7
Alexey <atereshhenko@gmail.com>
2022-02-04 20:09:32 +0300

    чуток подправил...

---
+++ Dashboard/Controllers/ExamplesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Controllers
+{
+	public class ExamplesController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}		
+	}
+}
